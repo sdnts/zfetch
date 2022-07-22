2a568c456f0f59987c472e601f0fc275b41c50b7
Siddhant <siddhant@fastmail.com>
2022-07-22 17:56:15 +0200

    improve: simplify Decorated

--- src/fmt.zig
+++ src/fmt.zig
@@ -5,70 +5,63 @@ const Writer = std.fs.File.Writer;
 
 // Reference: https://en.wikipedia.org/wiki/ANSI_escape_code#8-bit
 
-const Style = enum(u8) { Regular = 0, Bold = 8 };
+const Style = enum(u8) { Regular, Bold };
 
 // While technically we can support full 24-bit colors (RGB) because most modern terminals will be able to render them, there's really no point, the standard 256 colors are more than enough
 const Color = enum(u8) { Black, Red, Green, Blue, Magenta, Cyan, White };
 
-pub fn Decorated(comptime Value: type) type {
-    return struct {
-        const Self = @This();
+pub const Decorated = struct {
+    const Self = @This();
 
-        value: Value,
-        style: Style = .Regular,
-        fg: ?Color = null,
-        bg: ?Color = null,
+    style: Style = .Regular,
+    fg: ?Color = null,
+    bg: ?Color = null,
 
-        pub fn init(value: Value) Self {
-            return .{ .value = value };
+    pub fn init() Self {
+        return .{};
+    }
+
+    pub fn bold(self: *Self) *Self {
+        self.style = .Bold;
+        return self;
+    }
+
+    pub fn regular(self: *Self) *Self {
+        self.style = .Regular;
+        return self;
+    }
+
+    pub fn foreground(self: *Self, color: Color) *Self {
+        self.fg = color;
+        return self;
+    }
+
+    pub fn background(self: *Self, color: Color) *Self {
+        self.bg = color;
+        return self;
+    }
+
+    pub fn print(self: *Self, writer: Writer, comptime format: []const u8, args: anytype) !void {
+        switch (self.style) {
+            .Bold => try writer.print("\x1B[1m", .{}),
+            else => {},
         }
 
-        pub fn bold(self: *Self) *Self {
-            self.style = .Bold;
-            return self;
+        if (self.fg) |color| {
+            try writer.print("\x1B[38;5;{d}m", .{@enumToInt(color)});
         }
 
-        pub fn regular(self: *Self) *Self {
-            self.style = .Regular;
-            return self;
+        if (self.bg) |color| {
+            try writer.print("\x1B[48;5;{d}m", .{@enumToInt(color)});
         }
 
-        pub fn foreground(self: *Self, color: Color) *Self {
-            self.fg = color;
-            return self;
-        }
+        try writer.print(format, args);
 
-        pub fn background(self: *Self, color: Color) *Self {
-            self.fg = color;
-            return self;
-        }
+        try writer.print("\x1B[0m", .{});
+    }
 
-        pub fn print(self: *Self, writer: Writer) !void {
-            switch (self.style) {
-                .Bold => try writer.print("\x1B[1m", .{}),
-                else => {},
-            }
-
-            if (self.fg) |color| {
-                try writer.print("\x1B[38;5;{d}m", .{@enumToInt(color)});
-            }
-
-            if (self.bg) |color| {
-                try writer.print("\x1B[48;5;{d}m", .{@enumToInt(color)});
-            }
-
-            switch (@typeInfo(Value)) {
-                .Int => try writer.print("{d}", .{self.value}),
-                .Float => try writer.print("{d}", .{self.value}),
-                else => try writer.print("{s}", .{self.value}),
-            }
-
-            try writer.print("\x1B[0m", .{});
-        }
-
-        pub fn println(self: *Self, writer: Writer) !void {
-            try self.print(writer);
-            try writer.print("\n", .{});
-        }
-    };
-}
+    pub fn println(self: *Self, writer: Writer, comptime format: []const u8, args: anytype) !void {
+        try self.print(writer, format, args);
+        try writer.print("\n", .{});
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -3,6 +3,8 @@ const builtin = @import("builtin");
 const impl = @import("impl.zig");
 const fmt = @import("fmt.zig");
 
+const Decorated = fmt.Decorated;
+
 pub fn main() !void {
     var arena = std.heap.ArenaAllocator.init(std.heap.raw_c_allocator);
     var allocator = arena.allocator();
@@ -10,72 +12,63 @@ pub fn main() !void {
 
     const stdout = std.io.getStdOut().writer();
 
-    const DecoratedString = fmt.Decorated([]const u8);
-    const DecoratedUsize = fmt.Decorated(usize);
-
     var cores = try impl.cores(allocator);
-    try DecoratedString.init("Cores: ").bold().print(stdout);
-    try DecoratedUsize.init(cores).println(stdout);
+    try Decorated.init().bold().print(stdout, "Cores: ", .{});
+    try Decorated.init().println(stdout, "{d}", .{cores});
 
     const cpu = try impl.cpu(allocator);
-    try DecoratedString.init("CPU: ").bold().print(stdout);
-    try DecoratedString.init(cpu).println(stdout);
+    try Decorated.init().bold().print(stdout, "CPU: ", .{});
+    try Decorated.init().println(stdout, "{s}", .{cpu});
 
     const gpus = try impl.gpu(allocator);
-    try DecoratedString.init("GPU: ").bold().print(stdout);
+    try Decorated.init().bold().print(stdout, "GPU: ", .{});
     if (gpus.items.len == 0) {
-        try DecoratedString.init("Integrated").println(stdout);
+        try Decorated.init().println(stdout, "Integrated", .{});
     } else for (gpus.items) |gpu| {
-        try DecoratedString.init(gpu).println(stdout);
+        try Decorated.init().println(stdout, "{s}", .{gpu});
     }
 
     const hostname = try impl.hostname();
-    try DecoratedString.init("Hostname: ").bold().print(stdout);
-    try DecoratedString.init(hostname[0..]).println(stdout);
+    try Decorated.init().bold().print(stdout, "Hostname: ", .{});
+    try Decorated.init().println(stdout, "{s}", .{hostname});
 
     const kernel = try impl.kernel(allocator);
-    try DecoratedString.init("Kernel: ").bold().print(stdout);
-    try DecoratedString.init(kernel).println(stdout);
+    try Decorated.init().bold().print(stdout, "Kernel: ", .{});
+    try Decorated.init().println(stdout, "{s}", .{kernel});
 
     const machine = try impl.machine(allocator);
-    try DecoratedString.init("Machine: ").bold().print(stdout);
-    try DecoratedString.init(machine).println(stdout);
+    try Decorated.init().bold().print(stdout, "Machine: ", .{});
+    try Decorated.init().println(stdout, "{s}", .{machine});
 
     const os = try impl.os(allocator);
-    try DecoratedString.init("OS: ").bold().print(stdout);
-    try DecoratedString.init(os).println(stdout);
+    try Decorated.init().bold().print(stdout, "OS: ", .{});
+    try Decorated.init().println(stdout, "{s}", .{os});
 
     const ram = try impl.ram(allocator);
-    try DecoratedString.init("RAM: ").bold().print(stdout);
-    try DecoratedUsize.init(ram / (1024 * 1024 * 1024)).println(stdout);
+    try Decorated.init().bold().print(stdout, "RAM: ", .{});
+    try Decorated.init().println(stdout, "{d}GB", .{ram / (1024 * 1024 * 1024)});
 
     const resolution = try impl.resolution();
-    try stdout.print("Resolution: {d}x{d}\n", .{ resolution.width, resolution.height });
+    try Decorated.init().bold().print(stdout, "Resolution: ", .{});
+    try Decorated.init().println(stdout, "{d}x{d}", .{ resolution.width, resolution.height });
 
     const shell = try impl.shell();
-    try DecoratedString.init("Shell: ").bold().print(stdout);
-    try DecoratedString.init(shell).println(stdout);
+    try Decorated.init().bold().print(stdout, "Shell: ", .{});
+    try Decorated.init().println(stdout, "{s}", .{shell});
 
     const term = try impl.term();
-    try DecoratedString.init("Term: ").bold().print(stdout);
-    try DecoratedString.init(term).println(stdout);
+    try Decorated.init().bold().print(stdout, "Term: ", .{});
+    try Decorated.init().println(stdout, "{s}", .{term});
 
     const threads = try impl.threads(allocator);
-    try DecoratedString.init("Threads: ").bold().print(stdout);
-    try DecoratedUsize.init(threads).println(stdout);
+    try Decorated.init().bold().print(stdout, "Threads: ", .{});
+    try Decorated.init().println(stdout, "{d}", .{threads});
 
     const uptime = try impl.uptime(allocator);
-    try DecoratedString.init("Uptime: ").bold().print(stdout);
-    try DecoratedUsize.init(uptime.days).print(stdout);
-    try DecoratedString.init("d ").print(stdout);
-    try DecoratedUsize.init(uptime.hours).print(stdout);
-    try DecoratedString.init("h ").print(stdout);
-    try DecoratedUsize.init(uptime.minutes).print(stdout);
-    try DecoratedString.init("m ").print(stdout);
-    try DecoratedUsize.init(uptime.seconds).print(stdout);
-    try DecoratedString.init("s").println(stdout);
+    try Decorated.init().bold().print(stdout, "Uptime: ", .{});
+    try Decorated.init().println(stdout, "{d}d {d}h {d}m {d}s", .{ uptime.days, uptime.hours, uptime.minutes, uptime.seconds });
 
     const user = try impl.user();
-    try DecoratedString.init("User: ").bold().print(stdout);
-    try DecoratedString.init(user).println(stdout);
+    try Decorated.init().bold().print(stdout, "User: ", .{});
+    try Decorated.init().println(stdout, "{s}", .{user});
 }
